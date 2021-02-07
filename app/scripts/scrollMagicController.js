cb783b1e5956b48e4b69dc0ad6a55d4646adebc1
Eric Ranschau <eranschau@mac.com>
2021-02-06 18:29:21 -0600

    - More animations.

--- app/scripts/scrollMagicController.js
+++ app/scripts/scrollMagicController.js
@@ -1,23 +1,33 @@
 $(function(){
   var CONSTANTS = {
-    reverse: false,
-    triggerHook: 0.8
+    addIndicators: true,
+    offset: 0,
+    reverse: true,
+    scene: 'section',
+    toggleClass: 'visible',
+    triggerHook: 0.60,
   };
 
-  var controller = new ScrollMagic.Controller();
+  var controller = new ScrollMagic.Controller({ addIndicators: CONSTANTS.addIndicators });
 
-  var $sections = $('section');
+  var $scenes = $(CONSTANTS.scene);
 
   var scene_onEnter = function(event) {
     var $element = $(event.target.triggerElement());
 
-    $element.addClass('visible');
+    $element.addClass(CONSTANTS.toggleClass);
   };
 
   var scene_onEnter_clients = function(event) {
+    var $body = $('body');
+    var $element = $(event.target.triggerElement());
+
+    $body.removeClass('background-storyboards');
+
+    if ($element.hasClass(CONSTANTS.toggleClass)) { return; }
+
     var delay = 100;
 
-    var $element = $(event.target.triggerElement());
     var $listElements = $('.list > li', $element);
 
     $listElements.each(function(idx, el) {
@@ -27,15 +37,16 @@ $(function(){
         $el.addClass('visible');
       }, delay * idx);
     });
-
-    $element.addClass('visible');
   };
 
   var scene_onEnter_hero = function(event) {
+    var $element = $(event.target.triggerElement());
+
+    if ($element.hasClass(CONSTANTS.toggleClass)) { return; }
+
     var delay = 300;
     var offset = 600;
 
-    var $element = $(event.target.triggerElement());
     var $span = $('span.font-italic', $element);
 
     $span.each(function(idx, el) {
@@ -45,13 +56,16 @@ $(function(){
         $el.addClass('visible');
       }, offset + delay * idx);
     });
-
-    $element.addClass('visible');
   };
 
   var scene_onEnter_storyboard = function(event) {
+    var $body = $('body');
     var $element = $(event.target.triggerElement());
-    var $container = $element.parent();
+
+    $body.addClass('background-storyboards');
+
+    if ($element.hasClass(CONSTANTS.toggleClass)) { return; }
+
     var $img = $('img', $element);
     var $mask = $('.mask', $element);
 
@@ -65,39 +79,66 @@ $(function(){
       $mask.delay(1000).animate({
         height: '0%',
       }, 600, function() {
-        $container.addClass('visible');
-
         $img.addClass('shadowed');
       });
     });
-
-    $element.addClass('visible');
   };
 
-  $.each($sections, function(i, section) {
+  var scene_onStart = function(event) {
+
+    if (event.scrollDirection !== 'REVERSE') { return; }
+
+    var $body = $('body');
+    var $element = $(event.target.triggerElement());
+    var $previousElement = $element.prev();
+
+    if ($element.hasClass('storyboard') && $previousElement.hasClass('hero')) {
+      $body.removeClass('background-storyboards');
+    }
+
+    if ($element.hasClass('clients') && $previousElement.hasClass('storyboard')) {
+      $body.addClass('background-storyboards');
+    }
+  };
+
+  $.each($scenes, function(i, section) {
     var scene = new ScrollMagic.Scene({
-        offset: CONSTANTS.offset,
-        reverse: CONSTANTS.reverse,
-        triggerElement: section,
-        triggerHook: CONSTANTS.triggerHook
+      offset: CONSTANTS.offset,
+      reverse: CONSTANTS.reverse,
+      triggerElement: section,
+      triggerHook: CONSTANTS.triggerHook
     })
-    .addTo(controller)
+    .addIndicators({
+      name: section.className,
+      indent: 520,
+      colorEnd: 'blue',
+      colorStart: 'red',
+      colorTrigger: 'red',
+    })
     .on('enter', function(event) {
       switch(section.className) {
         case 'clients':
+        case 'clients visible':
           scene_onEnter_clients(event);
+          scene_onEnter(event);
           break;
         case 'hero':
           scene_onEnter_hero(event);
+          scene_onEnter(event);
           break;
         case 'storyboard':
+        case 'storyboard visible':
           scene_onEnter_storyboard(event);
+          scene_onEnter(event);
           break;
-
         default:
           scene_onEnter(event);
           break;
       }
-    });
+    })
+    .on('start', function(event) {
+      scene_onStart(event);
+    })
+    .addTo(controller);
   });
 });
